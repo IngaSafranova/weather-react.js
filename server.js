60253f47cc71e025670ddf451cb23be8c9dd61a3
ingasafranova <sfrnv@yahoo.co.uk>
2022-06-28 12:26:14 +0100

    fetch request with axios created and working

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
+import dotenv from "dotenv";
+import express from "express";
+import axios from "axios";
+import { response } from "express";
 
-    import dotenv from 'dotenv';
-    import express from 'express';
-    
-    let config = dotenv.config();
+let config = dotenv.config();
 
-    export const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
-    const app = express();
+export const API_KEY = process.env.API_KEY;
+console.log(API_KEY);
+const app = express();
 
-    app.use(express.json());
-    app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static("public"));
 
-    app.post('/weather',function(req,res){
-            console.log(req.body)
-    });
+app.post("/weather", function (req, res) {
+  const url = ` http://api.openweathermap.org/geo/1.0/direct?q=${req.body.city}&limit=5&appid=${process.env.API_KEY}`;
+const config = {responseType:'json'}
+  axios(  url, config ).then((response) => {
+  const lat = response.data[0].lat
+  const lon = response.data[0].lon
+   
+console.log(lat)
 
-    app.listen(3000,()=>{
-        console.log('Server started')
-    })
+return axios(`
+         https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.API_KEY}`,config)
+.then((response)=>res.json(response.data))})
+
+
+
+  //           return fetch(`
+  //           https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+  //             .then((res) => res.json())
+  //             .then((data) => {
+  //               console.log(data);
+  //               setWeather({ temp: data.current.temp,
+  //                             weather: data.current.weather[0].description });
+
+  //             });
+  //         });
+  //         setCity("");
+
+  //         console.log({city})
+});
+
+app.listen(3001, () => {
+  console.log("Server started");
+});

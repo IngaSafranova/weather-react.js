97653d7c8636c62ca5280ab26b4a4e1d47719d0c
ingasafranova <sfrnv@yahoo.co.uk>
2022-06-29 09:52:01 +0100

    info is rendering

--- public/script.js
+++ public/script.js
@@ -1,6 +1,47 @@
 
 const enter = document.querySelector('.btn');
 //console.log(enter)
+const locationElement = document.querySelector('.location');
+//console.log(locationElement)
+    const dateElement = document.querySelector('.date');
+   const weatherElement = document.querySelector(".weather-value");
+   //console.log(weatherElement)
+    const temperatureElement = document.querySelector('.temperature-value');
+    //console.log(temperatureElement)
+    const humidityElement = document.querySelector('.humidity-value');
+//console.log(humidityElement)
+
+const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
 
 enter.addEventListener('click',(event)=>{
     event.preventDefault();
@@ -18,17 +59,37 @@ fetch ('/weather',{
         'Accept': 'application/json'
     },
     body: JSON.stringify({city})
-}).then(res =>res.json()).then(data =>{console.log(data)})
+})
+.then(res => { if(res.ok){ return res.json();
+} else{
+    throw new Error("Network response Error");
+}
+}).then(data =>{console.log(data)
+
+
+
+getWeather(city,data)
+    
+
+})
+.catch((error)  => console.log("Fetch error:", error));
 })
 
-const locationElement = document.querySelector('.location');
-const dateElement = document.querySelector('.date');
-const weatherElement = document.querySelector('.weather-status');
-const temperatureElement = document.querySelector('.temperature-status');
-const humidityElement = document.querySelector('.humidity-status');
+function getWeather(city,data){
+     const weather = data.current.weather[0].description
+ console.log(weather)
 
-function setWeather(data,city){
+ const temperature = Math.floor(data.current.temp)
+ console.log(temperature)
+ const humidity = `${data.current.humidity }%`
+ console.log(humidity)
+ 
+
     locationElement.textContent = city
-    dateElement.textContent =new Date()
-    weatherElement.textContent = data.current.weather[0].description
+    dateElement.textContent =dateBuilder(new Date())
+    weatherElement.textContent = weather;
+    temperatureElement.textContent = temperature;
+    humidityElement.textContent = humidity;
+
 }
+
